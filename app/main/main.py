76f534afb8a95441ba19263000ecadd8b5631c3b
cimpomir <mircea.cimpoi@gmail.com>
2019-11-23 19:10:51 +0100

    Added createUser api functionality

--- app/main/main.py
+++ app/main/main.py
@@ -1,31 +1,38 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, Blueprint
 from model.Teacher import Teacher
 from model.Admin import Admin
 from model.User import User
 
 app = Flask(__name__)
 
-#for testin
-userObject={'userID':'-1','username':'test'}
+# for testin
+userObject = {'userID': '-1', 'username': 'test'}
 
-@app.route('/')
+api_bp = Blueprint('api_bp', __name__)
+
+
+@api_bp.route('/')
 def index():
     return "Hello, World!"
 
-@app.route('/testUser')
+
+@api_bp.route('/testUser')
 def createTestNewUser():
     return 'Created Test New Users'
 
-@app.route('/createUser')
+
+@api_bp.route('/createUser')
 def createNewUser():
     output = User.createUser(userObject)
     return output
 
+
 @app.route('/userCreateNewUser')
-def userCreateNewUser():
+def userCreateNewUser(user_object):
     output = Admin.userCreateNewUser(userObject)
     return output
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/main/model/User.py
+++ app/main/model/User.py
@@ -1,8 +1,13 @@
+import json
+
 from .. import db
 
 USERNAME_LENGTH = 128
 EMAIL_LENGTH = 128
 PASSWORD_HASH_LENGTH = 256
+
+USER_TYPE_TEACHER = 1
+USER_TYPE_ADMIN = 1729
 
 
 class User(db.Model):
@@ -13,3 +18,33 @@ class User(db.Model):
     active = db.Column(db.Integer, nullable=False)  # put 0 or 1
     password = db.Column(db.String(PASSWORD_HASH_LENGTH))
     # TODO: support picture
+
+    @staticmethod
+    def createUser(user_details: dict) -> str:
+        """Receives dict (json?) with details of user to create
+        minimum: username, email, password
+        @return string with status (?)
+        """
+        username = user_details.get('username', None)
+        if username is None:
+            return 'No username provided'
+        email = user_details.get('email', None)
+        if email is None:
+            return 'No email was provided'
+
+        # should be only one
+        user_by_name = User.query.filter_by(username=username).first()
+        if user_by_name is not None:
+            return 'User already exists'
+
+        user_by_email = User.query.filter_by(username=email).first()
+        if user_by_email is not None:
+            return 'Email already registered'
+
+        new_user = User(email=email, username=username, password=user_details['password'],
+                        usertype=USER_TYPE_TEACHER, active=1)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return 'Created user'
